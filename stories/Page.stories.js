3cbeccf035dd94e46fda428a6ccf6fc2d67ed1fc
Hyerin <hyerin.bcit@gmail.com>
2021-02-26 12:15:00 +0900

    country quiz and recyclebin part

--- stories/Page.stories.js
+++ stories/Page.stories.js
@@ -6,7 +6,11 @@ import {Magnify} from '../comps/MangifyingGlass'
 //Phone (kelly)
 import {Phone} from '../comps/Phone'
 
+//country (hyerin)
+import {Quiz} from '../comps/CountryQuiz';
 
+//recyclebin (hyerin)
+import {RecycleBin} from '../comps/RecycleBin';
 
 //DEFAULT
 export default {
@@ -20,3 +24,5 @@ export default {
 export const Microplastic = () => Magnify
 
 export const PhoneQuiz = () => Phone
+export const MyQuiz = () => Quiz;
+export const MyRecycleBin = () => RecycleBin;
