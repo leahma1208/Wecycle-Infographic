6622ea609f1806d3c9b44dc2a919cd3a8a5a2fea
hannahcabug <hannahcabug@gmail.com>
2021-03-01 22:10:46 -0800

    common items and garden

---
+++ common-items/src/workflow.js
@@ -0,0 +1,9 @@
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/bottle.png", "Beverage Bottles", 0);
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/can.png",  "Beverage Cans");
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/caps.png", "Caps and Lips", 0);
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/cigarette.png",  "Cigarettes", 10);
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/mask.png", "Face Masks", -20);
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/plasticbag.png", "Plastic Bags", -20);
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/straw.png", "Straws");
+document.querySelector(".items").innerHTML += ItemsUI("./imgs/wrapper.png", "Food Wrapper", 150);
+document.querySelector(".garden").innerHTML += GardenUI();
---
+++ common-items/stories/comps.stories.js
@@ -0,0 +1,9 @@
+import {Items} from '../comps/Items';
+import {Garden} from '../comps/Garden';
+
+export default{
+    title:"My Components"
+}
+
+export const MyItems = () => Items;
+export const MyGarden = () => Garden;
