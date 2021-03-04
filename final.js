cce667a2e6bd6f8cda1b76673688ae945bcea88e
Hyerin <hyerin.bcit@gmail.com>
2021-03-04 19:41:25 +0900

    recycle part 2 and microplastic header

--- final.js
+++ final.js
@@ -17,7 +17,7 @@ document.querySelector("#hover").innerHTML += BodyTextUI("Hover Over the Bag!")
 document.querySelector(".bag1").innerHTML += PlasticBagUI()
 
 
-//recycle part
+//recycle part 1
 document.querySelector("#bin-1").innerHTML += RecycleBinUI('./public/images/gray_bin.png','Garbage');
 document.querySelector("#bin-2").innerHTML += RecycleBinUI('./public/images/blue_bin.png','Recycle');
 document.querySelector("#bin-3").innerHTML += RecycleBinUI('./public/images/green_bin.png','Compost');
@@ -31,6 +31,19 @@ document.querySelector("#wendy-whale").innerHTML += WendyWhale()
 document.querySelector("#wendy-par-1").innerHTML += BodyTextUI("<strong>Wendy the Whale</strong> might <br> <strong>mistakenly</strong> eat the plastic debris")
 document.querySelector("#wendy-par-2").innerHTML += BodyTextUI("<strong>Click on the right waste bin</strong> <br> to help Wendy!")
 
+//recycle part 2
+document.querySelector("#bin-4").innerHTML += RecycleBinUI('./public/images/gray_bin.png','Garbage');
+document.querySelector("#bin-5").innerHTML += RecycleBinUI('./public/images/blue_bin.png','Recycle');
+document.querySelector("#bin-6").innerHTML += RecycleBinUI('./public/images/green_bin.png','Compost');
+
+document.querySelector("#recycleBin-answer3").innerHTML += RecycleTrue()
+document.querySelector("#recycleBin-answer4").innerHTML += RecycleFalse()
+
+//magnifying part
+document.querySelector(".what_microplastic").innerHTML += SubCatUI("FIRST OFF... WHAT IS <br> MICROPLASTIC?");
+// document.querySelector("#magnify").innerHTML += MagnifyingGlass();
+
+
 //additional js
 
 //recycle part
@@ -43,6 +56,13 @@ function RecycleBinTrue(){
     document.querySelector("#fish-par-2").style.display = "none"
     document.querySelector("#fish-par-3").style.display = "block"
     document.querySelector("#hover").style.opacity = "0"
+    
+}
+
+function RecycleBinTrue_2(){
+    document.querySelector("#recycleBin-answer3").style.display = "block"
+    document.querySelector("#recycleBin-answer3").style.opacity = "1"
+    document.querySelector("#recycleBin-answer4").style.display = "none"
 }
 
 function RecycleBinFalse(){
@@ -54,4 +74,11 @@ function RecycleBinFalse(){
     document.querySelector("#fish-par-3").style.display = "none"
     document.querySelector(".bag1").style.opacity = "1"
     document.querySelector("#hover").style.opacity = "1"
+    
+}
+
+function RecycleBinFalse_2(){
+    document.querySelector("#recycleBin-answer3").style.display = "none"
+    document.querySelector("#recycleBin-answer3").style.opacity = "1"
+    document.querySelector("#recycleBin-answer4").style.display = "block"
 }
