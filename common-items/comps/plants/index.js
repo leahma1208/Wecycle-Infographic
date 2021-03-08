d9f8a6d72967cd22d304d6a8cd3a6969e48528a9
KellyMenzul <kmenzul@my.bcit.ca>
2021-03-07 23:39:29 -0800

    showed graden

--- common-items/comps/plants/index.js
+++ common-items/comps/plants/index.js
@@ -2,35 +2,63 @@ function PlantsUI()
 {
     return`
     <div style='
-    postion:absolute;
     display: flex;
+    width:100%;
     flex-direction: row;
+    justify-content:center;
+    background-color:#c5e5c8;
     '
     >
         <div style='
         display: flex;
         '
         >
+        <div
+        class="plant-holder"
+        style='
+        display:flex;
+        flex:1;
+        width:100%;
+        '
+        >
         <img style='
+        display:flex;
         width: 15rem;
         height: 15rem;
-        margin-right: 13rem;
         '
-        src="./imgs/plant.png"/>
+        src="../common-items/imgs/plant.png"/>
+        </div>
  
+        <div
+        class="plant-holder"
+        style='
+        display:flex;
+        flex:1;
+        width:100%;
+        '
+        >
         <img style='
         width: 15rem;
         height: 15rem;
-        margin-right: 13rem;
         '
-        src="./imgs/plant.png"/>
+        src="../common-items/imgs/plant.png"/>
+        </div>
  
+        <div
+        class="plant-holder"
+        style='
+        display:flex;
+        flex:1;
+        width:100%;
+        '
+        >
         <img style='
         width: 15rem;
         height: 15rem;
-        margin-right: 13rem;
         '
-        src="./imgs/plant.png"/>
+        src="../common-items/imgs/plant.png"/>
+        </div>
+
         </div>
  
     </div>
